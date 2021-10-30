3b910ff740ab888b9acae7c63ae8e0d4908f4e75
Philipp Schmitt <philipp@schmitt.co>
2021-10-30 14:51:55 +0200

    Zoom: look for hidden windows as well

--- tafker/snowflakes.py
+++ tafker/snowflakes.py
@@ -15,7 +15,9 @@ def zoom_meeting_status() -> Optional[tuple[psutil.Process, Window]]:
         return
 
     zoom_wins = find_windows(
-        cls="zoom", name="(Zoom Meeting)|(zoom_linux_float_video_window)"
+        cls="zoom",
+        name="(Zoom Meeting)|(zoom_linux_float_video_window)",
+        only_visible=False,
     )
     LOGGER.debug(f"Zoom PID: {zoom_proc.pid} - Windows: {zoom_wins}")
     return (zoom_proc, zoom_wins[0]) if zoom_wins else None
