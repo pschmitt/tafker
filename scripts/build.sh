582c7fecef5fa437c8fa77a70d82a6b6a5ce4944
Philipp Schmitt <philipp@schmitt.co>
2021-10-30 23:19:17 +0200

    Add pyinstaller build option

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-build() {
+build_pyoxidizer() {
   # pyoxidizer build --target-triple x86_64-unknown-linux-musl
   pyoxidizer build
 }
 
+build_pyinstaller() {
+  local python_v="${PYTHON_VERSION:-3.9}"
+
+  cd "$(git rev-parse --show-toplevel)" || exit 9
+
+  docker run -it --rm \
+    -v "${PWD}:/app" \
+    -e OWNER=1000 \
+    -e STATICX=1 \
+    -e STATICX_ARGS="--strip" \
+    "pschmitt/pyinstaller:${python_v}" \
+    "$@"
+}
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-  build
+  case "$1" in
+    pyinstaller)
+      build_pyinstaller
+      ;;
+    *)
+      build_pyoxidizer
+      ;;
+  esac
 fi
