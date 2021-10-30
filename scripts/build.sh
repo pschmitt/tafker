0b08a7249ea95789462e4886a22e4fa8186dcdb0
Philipp Schmitt <philipp@schmitt.co>
2021-10-30 23:26:29 +0200

    Generate requirements.txt before build

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,6 +21,9 @@ build_pyinstaller() {
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
+  cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
+  ./gen-requirements.sh
+
   case "$1" in
     pyinstaller)
       build_pyinstaller
